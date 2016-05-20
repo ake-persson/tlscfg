603025010483ef6239b6a7aa403191c417a6aa10
Michael Persson <michael.ake.persson@gmail.com>
2016-05-20 12:02:15 -0500

    Add system ca

--- tlscfg_linux.go
+++ tlscfg_linux.go
@@ -2,8 +2,32 @@ package tlscfg
 
 import (
 	"crypto/tls"
+	"errors"
+	"io/ioutil"
+	"os"
 )
 
 func appendSystemCerts(c *tls.Config) error {
-	return nil
+	certFiles := []string{
+		"/etc/ssl/certs/ca-certificates.crt", // Debian/Ubuntu/Gentoo etc.
+		"/etc/pki/tls/certs/ca-bundle.crt",   // Fedora/RHEL
+		"/etc/ssl/ca-bundle.pem",             // OpenSUSE
+		"/etc/pki/tls/cacert.pem",            // OpenELEC
+	}
+
+	for _, f := range certFiles {
+		if _, err := os.Stat(f); os.IsNotExist(err) {
+			continue
+		}
+
+		ca, err := ioutil.ReadFile(f)
+		if err != nil {
+			return err
+		}
+
+		c.RootCAs.AppendCertsFromPEM(ca)
+		return nil
+	}
+
+	return errors.New("can't locate system CA certificates")
 }
